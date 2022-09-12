ff07e9837e4ac7ab67687ddd70f8e1b9e19c7481
rushil <20it164@charusat.edu.in>
2022-09-12 13:58:03 +0530

    first

--- Frontend/webapps/src/components/Register.js
+++ Frontend/webapps/src/components/Register.js
@@ -29,6 +29,9 @@ function Register() {
                     <Button variant="primary" type="submit" className='my-3'>
                         Submit
                     </Button>
+                    <Button variant="primary" type="submit" className='my-3'>
+                        Submit
+                    </Button>
                 </Form>
             </Container>
             <Card.Footer>Thank You</Card.Footer>
